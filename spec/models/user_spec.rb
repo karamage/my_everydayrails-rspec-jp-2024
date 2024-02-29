130cf2603732ce4b0b2d5a65ea6de5c4363bcb24
karamage <karamage.umauma@gmail.com>
2024-02-29 14:32:39 +0900

    add first model spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  # 姓、名、メール、パスワードがあれば有効な状態であること
+  it "is valid with a first name, last name, email, and password" do
+    user = User.new(
+      first_name: "Aaron",
+      last_name:  "Sumner",
+      email:      "tester@example.com",
+      password:   "dottle-nouveau-pavilion-tights-furze",
+      )
+    expect(user).to be_valid
+  end
+
+end
