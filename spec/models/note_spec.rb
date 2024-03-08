65bcdd07ce5b0733804c08ed928e5c1ce883b8f1
karamage <karamage.umauma@gmail.com>
2024-03-08 16:19:04 +0900

    add note model spec

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -5,6 +5,31 @@ RSpec.describe Note, type: :model do
 
   # 検索結果が１件も見つからなければカラのコレクションを返すこと
   it "returns an empty collection when no results are found" do
+    user = User.create(
+      first_name:  "Joe",
+      last_name:  "Tester",
+      email:      "joetester@example.com",
+      password:   "dottle-nouveau-pavilion-tights-furze",
+    )
+
+    project = user.projects.create(
+      name: "Test Project",
+      )
+
+    note1 = project.notes.create(
+      message: "This is the first note.",
+      user: user,
+      )
+    note2 = project.notes.create(
+      message: "This is the second note.",
+      user: user,
+      )
+    note3 = project.notes.create(
+      message: "First, preheat the oven.",
+      user: user,
+      )
+
+    expect(Note.search("message")).to be_empty
   end
 
   # 検索文字列に一致するメモを返すこと
