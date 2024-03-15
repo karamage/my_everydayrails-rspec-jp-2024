81b916672a21baf6b36965df4d4c6eb8cf0ce29f
karamage <karamage.umauma@gmail.com>
2024-03-15 15:37:00 +0900

    add user factory bot with sequence email.

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -14,6 +14,13 @@ RSpec.describe Note, type: :model do
       )
   end
 
+  # ファクトリで関連するデータを生成する
+  it "generates associated data from a factory" do
+    note = FactoryBot.create(:note)
+    puts "This note's project is #{note.project.inspect}"
+    puts "This note's user is #{note.user.inspect}"
+  end
+
   it "is valid with a user, project, and message" do
     note = Note.new(
       message: "This is a sample note.",
