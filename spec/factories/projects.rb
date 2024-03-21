b31acce5faf3274bd26ed85c5bdb8ff7044ab9dd
karamage <karamage.umauma@gmail.com>
2024-03-21 11:03:49 +0900

    add project trait factory bot.

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -5,18 +5,18 @@ FactoryBot.define do
     due_on { 1.week.from_now }
     association :owner
 
-    # 昨日が締め切りのプロジェクト
-    factory :project_due_yesterday do
+    # 昨日が締め切り
+    trait :due_yesterday do
       due_on { 1.day.ago }
     end
 
-    # 今日が締め切りのプロジェクト
-    factory :project_due_today do
+    # 今日が締め切り
+    trait :due_today do
       due_on { Date.current.in_time_zone }
     end
 
-    # 明日が締め切りのプロジェクト
-    factory :project_due_tomorrow do
+    # 明日が締め切り
+    trait :due_tomorrow do
       due_on { 1.day.from_now }
     end
   end
