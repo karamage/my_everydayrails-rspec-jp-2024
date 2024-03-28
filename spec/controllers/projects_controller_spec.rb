36439a1eaea8f2af6df26c63bd229af208c2b8e2
karamage <karamage.umauma@gmail.com>
2024-03-28 17:30:57 +0900

    add project controller spec.

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -2,22 +2,40 @@ require 'rails_helper'
 
 RSpec.describe ProjectsController, type: :controller do
   describe "#index" do
-    before do
-      @user = FactoryBot.create(:user)
+    # 認証済みのユーザーとして
+    context "as an authenticated user" do
+      before do
+        @user = FactoryBot.create(:user)
+      end
+
+      # 正常にレスポンスを返すこと
+      it "responds successfully" do
+        sign_in @user
+        get :index
+        expect(response).to be_successful
+      end
+
+      # 200レスポンスを返すこと
+      it "returns a 200 response" do
+        sign_in @user
+        get :index
+        expect(response).to have_http_status "200"
+      end
     end
 
-    # 正常にレスポンスを返すこと
-    it "responds successfully" do
-      sign_in @user
-      get :index
-      expect(response).to be_successful
-    end
+    # ゲストとして
+    context "as a guest" do
+      # 302レスポンスを返すこと
+      it "returns a 302 response" do
+        get :index
+        expect(response).to have_http_status "302"
+      end
 
-    # 200レスポンスを返すこと
-    it "returns a 200 response" do
-      sign_in @user
-      get :index
-      expect(response).to have_http_status "200"
+      # サインイン画面にリダイレクトすること
+      it "resirects to the sign-in page" do
+        get :index
+        expect(response).to redirect_to "/users/sign_in"
+      end
     end
   end
 end
